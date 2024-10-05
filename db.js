dbfe17a1d23e547119ae207add573c1a975857a8
shalini19yadav <yadavsahalini007@gmail.com>
2024-10-05 12:41:11 +0530

    update

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 function connectToDB(){
-  console.log(process.env.MONGO_URI)
+  // console.log(process.env.MONGO_URI)
     mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('mongoose connected successfully'))
   .catch(()=>console.log('error in connection'))
